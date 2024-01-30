b3782d860f5c56bc8e90717ab6536d80f8197298
simoaica <simoaica@yahoo.com>
2024-01-30 16:18:00 +0200

    tab without typescript full

--- components/useClientOnlyValue.web.js
+++ components/useClientOnlyValue.web.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 // `useEffect` is not invoked during server rendering, meaning
 // we can use this to determine if we're on the server or not.
-export function useClientOnlyValue<S, C>(server: S, client: C): S | C {
-  const [value, setValue] = React.useState<S | C>(server);
+export function useClientOnlyValue(server, client) {
+  const [value, setValue] = React.useState(server);
   React.useEffect(() => {
     setValue(client);
   }, [client]);
